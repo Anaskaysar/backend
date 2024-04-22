3a414e768f83ac3bb514bbf7ecedee3c5943cd18
anaskaysar <kaysarul.apurba@northsouth.edu>
2024-04-22 10:02:09 -0400

    ip

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-// Replace "YOUR_API_KEY" with your actual WeatherAPI key
-const apiKey = "1746c67a89ad49a9afb135727241704";
-
 // Define a route to fetch weather data
 app.get('/weather', (req, res) => {
-  // Replace "YOUR_CITY" with the desired city (can be passed as a query parameter e.g., /weather?city=London)
   const city = req.query.city || "london";
-
+  // Replace "YOUR_API_KEY" with your actual WeatherAPI key
+  const apiKey = "1746c67a89ad49a9afb135727241704";
   const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}`;
 
   axios.get(apiUrl)
@@ -26,3 +23,5 @@ app.get('/weather', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
